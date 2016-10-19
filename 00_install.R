5b91048b83b85be02883cbb40314eb2146df5216
Joe Thorley <joe@poissonconsulting.ca>
2016-10-19 16:30:37 -0700

    Add 00_install.R script

---
+++ 00_install.R
@@ -0,0 +1,7 @@
+install.packages("devtools") # If you don't already have it installed
+
+library(devtools)
+install_github("bcgov/envreportutils")
+install_github("bcgov/bcmaps")
+install_github("bcgov/rems")
+install_github("bcgov/wqbc")
--- run_all.R
+++ run_all.R
@@ -10,6 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+source("00_install.R")
 source("01_load.R")
 source("02_clean.R")
 source("03_analysis.R")
