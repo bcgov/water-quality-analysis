9b84f257915cb3a34d44063ee0bff65523bd3fcf
Joe Thorley <joe@poissonconsulting.ca>
2016-10-20 09:40:01 -0700

    add install_packages function to only get if not installed

--- 00_install.R
+++ 00_install.R
@@ -1,8 +1,16 @@
-install.packages("devtools") 
-install.packages("magrittr") 
-install.packages("dplyr") 
+install_packages <- function(pkgs) {
+  stopifnot(is.character(pkgs))
+  for (pkg in pkgs) {
+    if (!require(pkg, character.only = TRUE, quietly = TRUE)) {
+      install.packages(pkg) 
+    }
+  }
+}
 
-library(devtools)
+install_packages("devtools") 
+install_packages("magrittr") 
+install_packages("dplyr") 
+
 install_github("bcgov/envreportutils")
 install_github("bcgov/bcmaps")
 install_github("bcgov/rems")
