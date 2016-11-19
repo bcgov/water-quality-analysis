745e98ab8091af80a228855a2a4bdcb0e57a11b9
colin millar <colinpmillar@gmail.com>
2016-11-19 10:07:45 +0100

    seasonal Mann kendal test

--- 03_analysis_trend.R
+++ 03_analysis_trend.R
@@ -14,5 +14,28 @@ source("header.R")
 
 ems <- readRDS("out/clean.rds")
 
-## analyse 
+## analyse trends for the 10-12 water quality parameters measured at 
+## 14 Federal-Provincial water quality monitoring stations using the 
+## Mann-Kendall Sen Slope analysis implemented in the 'zyp' 
+## R package. 
+
+# work on subset
+data <- filter(ems, Variable == "Copper Total")
+data <- filter(ems, Variable == "Copper Total" & Station_Number == "BC08MB0007")
+
+
+# nest
+ne_data <- data %>% nest(Date, Value, .key = Data)
+
+trend_test <- function(df) {
+  # only test for a trend if there is multiple years of data
+  # convert to monthly time series
+  df$Month <- month(df$Date)
+  df$Year <- year(df$Date)
+  df_ts <- df %>% with(., tapply(Value, list(Year, Month), mean)) %>% as.ts()
+  SeasonalMannKendall(df_ts)
+}
+
+ne_data %<>% mutate(Trend = map(Data, trend_test))
+ne_data
 
