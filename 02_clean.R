4ba8f8f54707023402dd691ad098309d6f8a5ab8
Joe Thorley <joe@poissonconsulting.ca>
2016-11-23 17:04:27 -0800

    playing with different thresholds

--- 02_clean.R
+++ 02_clean.R
@@ -15,47 +15,34 @@ source("header.R")
 ems <- readRDS("out/load.rds")
 provincial <- readRDS("out/provincial.rds")
 
+
 # for now just work on provincial data
 ems %<>% semi_join(provincial, by = c("Station_Number", "Code"))
+
+ems$Variable <- lookup_variables(ems$Code)
 
 # we need to eliminate clearly erroneous data before calling clean_wqdata.
 # we should also entirely eliminate variables at a site with insufficient data.
 # note we should not eliminate ph, hardness or chloride as required for limits on other variables.
 # Joe needs to check that detection limits are correct
 
+ems %<>% group_by(Station_Number, Code)
 
-ems %<>% dlply(c("Station_Number", "Code"))
-
-# drop data with less than 10 rows
-ems <- ems[vapply(ems, function(x) nrow(x) >= 10, TRUE)]
-
-# drop data with more than 10% outliers
-ems <- ems[vapply(ems, function(x) sum(x$is_outlier) / nrow(x) <= 0.1, TRUE)]
-
-ems %<>% ldply()
+# drop data time series with less than 10 values
+ems %<>% filter(n() >= 10)
+#ems %<>% ungroup()
 
 ## identify outliers
-ems %<>% outlier_id(by = c("Station_Number", "Code"))
-
-ems$Variable <- lookup_variables(ems$Code)
+ems %<>% outlier_id(by = c("Station_Number", "Code"), mad_threshold = 100, ts_threshold = 10)
 
 pdf()
 plot_timeseries_by(ems, by = c("Station_Number", "Variable", "Code", "Units"), color = "is_outlier")
 dev.off()
 
 
-# convert data to list of data frames by station number and code
-
-
-
-
-# remove problematic time series to short or too many identical?
-ems %<>% filter(!Station_Number == "BC08HB0018" & Code == "PB???T")
 
 ## remove outliers and drop is_outler column
 ems %<>% filter(!is_outlier) %>% select(-is_outlier)
-
-
 
 ## clean
 ems %<>% clean_wqdata(by = c("Station_Number"))
