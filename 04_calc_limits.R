f1b8b71e7f3234f1fcc5382f379e0f6fa9263fa4
joethorley <joe@poissonconsulting.ca>
2017-02-15 16:02:27 -0800

    plot seasonal

--- 04_calc_limits.R
+++ 04_calc_limits.R
@@ -19,6 +19,41 @@ load_object()
 soe %<>% inner_join(ecd, by = c("Station", "Variable")) %>%
   inner_join(select(stations, Station, Station_Name), by = "Station")
 
+soe %<>% mutate(Year = year(Date),
+                Dayte = Date)
+
+year(soe$Dayte) <- 1972
+
+values <- wqbc:::estimate_variable_values(soe, by = c("Station"))
+
+open_window(8,8)
+
+gp <- ggplot(data = filter(soe, Variable == "Hardness Total"), aes(x = Dayte, y = Value, group = Year, color = Year)) +
+  facet_wrap(~Station, scales = "free_y") + 
+  geom_point() +
+  geom_line(data = filter(values, Variable == "Hardness Total")) +
+  scale_x_date(date_labels = "%b") +
+  scale_y_continuous(name = "Hardness Total")
+
+print(gp)
+
+set_sub("seasonal")
+
+save_plot("Hardness Total", report = FALSE)
+
+open_window(8,8)
+
+gp <- ggplot(data = filter(soe, Variable == "pH"), aes(x = Dayte, y = Value, group = Year, color = Year)) +
+  facet_wrap(~Station, scales = "free_y") + 
+  geom_point() +
+  geom_line(data = filter(values, Variable == "pH")) +
+  scale_x_date(date_labels = "%b") +
+  scale_y_continuous(name = "pH")
+
+print(gp)
+
+save_plot("pH", report = FALSE)
+
 soe %<>% calc_limits(by = c("Station", "Station_Name"), term = "long-daily", estimate_variables = TRUE)
 
 set_sub("limits")
