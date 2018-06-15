4c422f928f1f4efc69f8ec4bc641ba37907d3960
Andy Teucher <andy.teucher@gov.bc.ca>
2018-06-15 15:52:00 -0700

    Dont' do filtering in the plotting function, expect the data to be pre-filtered

--- R/functions.R
+++ R/functions.R
@@ -1,12 +1,10 @@
-plot_station_vars <- function(data, station, x = "DateTime", y = "Value", 
+plot_station_vars <- function(data, title, x = "DateTime", y = "Value", 
                               var = "PARAM_SHORT_NAME", which = "all", ...) {
   
   x <- rlang::sym(x)
   y <- rlang::sym(y)
   var <- rlang::sym(var)
-  
-  data <- data %>% filter(SITE_NO == station)
-  
+
   if (!which == "all") {
     data <- filter(data, !!var %in% which)
   }
@@ -14,9 +12,10 @@ plot_station_vars <- function(data, station, x = "DateTime", y = "Value",
   
   ggplot(data, aes(x = !!x, y = !!y)) + 
     facet_wrap(vars(!!var), scales = "free_y") + 
-    geom_point(aes(colour = censored),size = 0.2) + 
+    geom_point(aes(colour = .data$censored),size = 0.2) + 
     geom_smooth(..., size = 0.2) + 
-    ggtitle(paste(data$PEARSEDA[1], station, sep = ": ")) + 
+    labs(title = paste(data$PEARSEDA[1], title, sep = ": "), 
+         colour = "Censored") + 
     theme(strip.text = element_text(size = rel(0.6)))
 }
 
@@ -27,3 +26,5 @@ make_censor <- function(x) {
     x == ">" ~ "right"
   )
 }
+
+
